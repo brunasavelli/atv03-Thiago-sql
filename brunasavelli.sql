cf9507abf8ec83022e78571b0e899165dce78c3f
Bruna Savelli <bnsavelli@gmail.com>
2024-11-05 11:38:46 -0300

    feat: consults done

--- brunasavelli.sql
+++ brunasavelli.sql
@@ -20,7 +20,7 @@ CREATE TABLE quartos (
 
 CREATE TABLE reservas (
     id_reserva SERIAL PRIMARY KEY,
-    data_entrada DATE NOT NULL,
+    data_entrada DATE,
     data_saida DATE NOT NULL,
     horario_entrada TIME NOT NULL,
     id_hospede INT NOT NULL,
@@ -58,7 +58,7 @@ INSERT INTO reservas (id_hospede, id_quarto, data_entrada, data_saida, horario_e
 (5, 5, '11-01-2025', '20-01-2025', '09:30'),
 (6, 6, '24-01-2025', '30-01-2025', '11:00'),
 (7, 7, '10-10-2024', '23-10-2024', '12:00'),
-(8, 8, '25-10-2024', '30-10-2025', '09:30');
+(8, 8, '25-10-2024', '30-10-2024', '09:30');
 
 -- Relacionamento
 SELECT
@@ -77,3 +77,74 @@ JOIN
     hospedes h ON r.id_hospede = h.id_hospede
 JOIN
     quartos q ON r.id_quarto = q.id_quarto;
+
+-- Consulta que lista apenas os hóspedes que já finalizaram suas estadias
+SELECT
+    r.id_reserva,
+    h.nome AS nome_hospede,
+    h.email AS email_hospede,
+    h.cpf AS cpf_hospede,
+    q.numero AS numero_quarto,
+    q.andar,
+    r.data_entrada,
+    r.data_saida,
+    r.horario_entrada
+FROM
+    reservas r
+JOIN
+    hospedes h ON r.id_hospede = h.id_hospede
+JOIN
+    quartos q ON r.id_quarto = q.id_quarto
+WHERE
+    r.data_saida < CURRENT_DATE;
+
+-- Consulta para mostrar todos os hóspedes, incluindo aqueles que ainda estão com reservas ativas.
+
+-- *Tive que inserir hospedes que estão ativos no hotel*
+INSERT INTO hospedes (nome, email, cpf) VALUES
+('Fernanda', 'fer@gmail.com', '999.999.999-99'),
+('Mini', 'mini@gmail.com', '000.000.000-00');
+
+INSERT INTO quartos (numero, andar) VALUES
+(13, 4),
+(14, 4);
+
+INSERT INTO reservas (id_hospede, id_quarto, data_entrada, data_saida, horario_entrada) VALUES
+(9, 9, '05-11-2024', '10-11-2024', '08:00'),
+(10, 10, '03-11-2024', '15-11-2024', '08:30');
+
+SELECT
+    r.id_reserva,
+    h.nome AS nome_hospede,
+    h.email AS email_hospede,
+    h.cpf AS cpf_hospede,
+    q.numero AS numero_quarto,
+    q.andar,
+    r.data_entrada,
+    r.data_saida,
+    r.horario_entrada
+FROM
+    reservas r
+JOIN
+    hospedes h ON r.id_hospede = h.id_hospede
+JOIN
+    quartos q ON r.id_quarto = q.id_quarto;
+
+-- Consulta que mostra apenas os quartos que não foram reservados ainda.
+
+-- *Inseri mais quartos e mudei a tabela*
+INSERT INTO quartos (numero, andar) VALUES
+(9, 3),
+(10, 3),
+(11, 3),
+(12, 3);
+
+SELECT
+    q.numero AS numero_quarto,
+    q.andar
+FROM
+    quartos q
+LEFT JOIN
+    reservas r ON q.id_quarto = r.id_quarto
+WHERE
+    r.id_quarto IS NULL;
